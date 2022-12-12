56e2c0b5310fe8c0d63a10792125f63c8bd44d5a
Reuben-Stephen-John <reuben.sj2003@gmail.com>
2022-12-12 22:03:05 +0530

    donor_model

---
+++ services/choices.py
@@ -0,0 +1,10 @@
+BLOOD_CHOICES=[
+    ('O +ve', 'O +ve'),
+    ('O -ve','O -ve',),
+    ('A +ve', 'A +ve',),
+    ('A -ve', 'A -ve',),
+    ('B +ve','B +ve',),
+    ('B -ve','B -ve',),
+    ('AB +ve','AB +ve'),
+    ('AB -ve', 'AB -ve'),
+]
--- services/models.py
+++ services/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .choices import *
 # Create your models here.
 
-    
+class Donor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    blood_type=models.CharField(max_length=100,choices=BLOOD_CHOICES)
+    address = models.TextField(max_length=300)
+    last_donated = models.DateField(blank=True)
+    def __str__(self) -> str:
+        return self.user.phonenumbers
+
