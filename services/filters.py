8b7e69507ac1d620e6eaddd3ac99cefe87547d37
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-01-05 12:48:44 +0530

    Filter Form

--- services/filters.py
+++ services/filters.py
@@ -3,8 +3,7 @@ from django_filters import ChoiceFilter, DateRangeFilter
 from .models import Donor
 
 class DonorFilter(django_filters.FilterSet):
-    address = django_filters.CharFilter(lookup_expr='icontains')
-    date_of_birth = DateRangeFilter(label='Last Donated')
+    address = django_filters.CharFilter(lookup_expr='icontains',label = "Address Lookup")
     class Meta:
         model = Donor
-        fields = ['blood_type']
+        fields = ['blood_type',]
