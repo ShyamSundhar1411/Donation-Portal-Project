bb29ae254793803700cee99f8aedb98d020511e7
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-01-06 16:06:45 +0530

    Types

--- services/forms.py
+++ services/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from .models import *
+from .choices import *
 from phonenumber_field.formfields import PhoneNumberField
 from django.contrib.auth.models import User
 
 class DonorForm(forms.ModelForm):
     role= forms.CharField(disabled=True)
     contact= PhoneNumberField(required=True)
-    compatible_types = forms.CharField(disabled=True,required=False)
+    compatible_types = forms.ChoiceField(disabled=True,required=False,choices = COMPATBILE_TYPES)
     class Meta:
         model=Donor
         fields=['role','blood_type','address','contact','date_of_birth','compatible_types']
--- services/templatetags/custom_tags.py
+++ services/templatetags/custom_tags.py
@@ -1,5 +1,6 @@
 from django import template
 import datetime
+from services.choices import COMPATBILE_TYPES
 
 register = template.Library()
 
@@ -7,5 +8,9 @@ def age(bday, d=None):
     if d is None:
         d = datetime.date.today()
     return (d.year - bday.year) - int((d.month, d.day) < (bday.month, bday.day))
-
+def split(strings,key):
+    for i in COMPATBILE_TYPES:
+        if i[0] == strings:
+            return i[-1].split(key)
 register.filter('age', age)
+register.filter("split",split)
