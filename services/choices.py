9d30ab1c26c583d61459fc5a7cce9ec71202fcad
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2023-01-07 10:54:18 +0530

    Compatible Types

--- services/choices.py
+++ services/choices.py
@@ -12,13 +12,13 @@ ROLE_CHOICES = [
     ("Donor","Donor"),
     ("Unauthorized", "Unauthorized"),
 ]
-COMPATBILE_TYPES=[
-    ("A +ve","A +ve,AB +ve"),
-    ("O +ve","O +ve,A +ve,B +ve,AB +ve"),
-    ("B +ve","B +ve,AB +ve"),
+COMPATIBLE_TYPES=[
+    ("A +ve,AB +ve","A +ve,AB +ve"),
+    ("O +ve,A +ve,B +ve,AB +ve","O +ve,A +ve,B +ve,AB +ve"),
+    ("B +ve,AB +ve","B +ve,AB +ve"),
     ("AB +ve","AB +ve"),
-    ("A -ve","A +ve,A -ve,AB -ve,AB +ve"),
-    ("O -ve","A +ve,A -ve,O -ve,O +ve,AB +ve,AB -ve,B +ve,B -ve"),
-    ("B -ve","B +ve,B -ve,AB -ve,AB +ve"),
-    ("AB -ve","AB +ve,AB -ve"), 
+    ("A +ve,A -ve,AB -ve,AB +ve","A +ve,A -ve,AB -ve,AB +ve"),
+    ("A +ve,A -ve,O -ve,O +ve,AB +ve,AB -ve,B +ve,B -ve","A +ve,A -ve,O -ve,O +ve,AB +ve,AB -ve,B +ve,B -ve"),
+    ("B +ve,B -ve,AB -ve,AB +ve","B +ve,B -ve,AB -ve,AB +ve"),
+    ("AB +ve,AB -ve","AB +ve,AB -ve"), 
 ]
